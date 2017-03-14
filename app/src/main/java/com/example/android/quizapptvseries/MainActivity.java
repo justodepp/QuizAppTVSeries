df25e853caf5701a769fe7d87185a9d284dca523
Gianni <gianniandreacavalli@gmail.com>
2017-03-14 17:39:56 +0100

    second Commit

--- app/src/main/java/com/example/android/quizapptvseries/MainActivity.java
+++ app/src/main/java/com/example/android/quizapptvseries/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.android.quizapptvseries;
 
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+    CollapsingToolbarLayout collapsingToolbar;
+    Toolbar toolbar;
+    ImageView first_ImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setActionOnScrollUp();
+
+        first_ImageView = (ImageView) findViewById(R.id.imageView);
+        Picasso.with(this)
+                .load(R.drawable.dexter)
+                .fit()
+                .centerCrop()
+                .into(first_ImageView);
+    }
+
+    //Set text on actionBar when scrolling up.
+    public void setActionOnScrollUp() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        collapsingToolbar.setTitle("TV Series - Quiz");
+        collapsingToolbar.setExpandedTitleColor(ResourcesCompat.getColor(getResources(), android.R.color.transparent, null));
     }
 }
