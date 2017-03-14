e38328269e1bc3c8d36aa340ad89c853a403040f
Gianni <gianniandreacavalli@gmail.com>
2017-03-14 16:08:18 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/quizapptvseries/SplashScreen.java
@@ -0,0 +1,42 @@
+package com.example.android.quizapptvseries;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+
+/**
+ * Created by gacavalli on 14/03/2017.
+ */
+
+public class SplashScreen extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.splashscreen);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+}
