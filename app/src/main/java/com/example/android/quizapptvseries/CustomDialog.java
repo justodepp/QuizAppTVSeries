cd0e84d91eabe7faabdac978e5256cf456484ba9
Gianni <gianniandreacavalli@gmail.com>
2017-03-17 18:13:16 +0100

    getName

--- app/src/main/java/com/example/android/quizapptvseries/CustomDialog.java
+++ app/src/main/java/com/example/android/quizapptvseries/CustomDialog.java
@@ -6,7 +6,6 @@ package com.example.android.quizapptvseries;
 
 import android.app.Activity;
 import android.app.Dialog;
-import android.content.Intent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
